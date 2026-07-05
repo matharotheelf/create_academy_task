2bf68cee69442f247f660e6b93a98c8969299a74
Tom Lawrence <mrthomasjacoblawrence@hotmail.co.uk>
2026-07-05 19:26:46 +0100

    Show page for mux video
    1. A show page to display mux video
    2. Renders page title

--- app/controllers/mux_videos_controller.rb
+++ app/controllers/mux_videos_controller.rb
@@ -2,4 +2,7 @@ class MuxVideosController < ApplicationController
   def index
     @mux_videos = MuxVideo.all
   end
+
+  def show
+  end
 end
---
+++ spec/features/mux_videos/mux_videos_show_spec.rb
@@ -0,0 +1,10 @@
+describe "Visit show page of mux video", type: :feature do
+  before :each do
+    mux_video_1 = create(:mux_video, provider_id: "PROVIDER-UID-1")
+    visit '/mux_videos/1'
+  end
+
+  it "displays index page title" do
+    expect(page).to have_content 'Watch Video'
+  end
+end
