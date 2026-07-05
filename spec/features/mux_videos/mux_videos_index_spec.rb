2fb268cd91e7f866148997a6d21354a8ec94b956
Tom Lawrence <mrthomasjacoblawrence@hotmail.co.uk>
2026-07-05 23:57:09 +0100

    Add list of link to mux video page

--- spec/features/mux_videos/mux_videos_index_spec.rb
+++ spec/features/mux_videos/mux_videos_index_spec.rb
@@ -1,16 +1,17 @@
 describe "Visit index page on homepage", type: :feature do
+  let!(:mux_video_1){ create(:mux_video, provider_id: "PROVIDER-UID-1") }
+  let!(:mux_video_2){ create(:mux_video, provider_id: "PROVIDER-UID-2") }
+
   before :each do
-    mux_video_1 = create(:mux_video, provider_id: "PROVIDER-UID-1")
-    mux_video_2 = create(:mux_video, provider_id: "PROVIDER-UID-2")
     visit '/'
   end
 
   it "displays mux video provider ID 1" do
-    expect(page).to have_content 'PROVIDER-UID-1'
+    page.should have_link(mux_video_1.provider_id, href: mux_video_path(mux_video_1))
   end
 
   it "displays mux video provider ID 2" do
-    expect(page).to have_content 'PROVIDER-UID-2'
+    page.should have_link(mux_video_2.provider_id, href: mux_video_path(mux_video_2))
   end
 
   it "displays index page title" do
