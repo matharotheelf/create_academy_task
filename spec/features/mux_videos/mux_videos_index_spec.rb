e7d6fd5841e0c5a58ea51cafaa5aa85d55265dd8
Tom Lawrence <mrthomasjacoblawrence@hotmail.co.uk>
2026-07-05 18:56:18 +0100

    Renders multiple mux videos UIDs

--- spec/features/mux_videos/mux_videos_index_spec.rb
+++ spec/features/mux_videos/mux_videos_index_spec.rb
@@ -1,11 +1,16 @@
 describe "Visit index page on homepage", type: :feature do
   before :each do
     mux_video_1 = create(:mux_video, provider_id: "PROVIDER-UID-1")
+    mux_video_2 = create(:mux_video, provider_id: "PROVIDER-UID-2")
     visit '/'
   end
 
   it "displays mux video provider ID 1" do
     expect(page).to have_content 'PROVIDER-UID-1'
+  end
+
+  it "displays mux video provider ID 2" do
+    expect(page).to have_content 'PROVIDER-UID-2'
   end
 
   it "displays index page title" do
