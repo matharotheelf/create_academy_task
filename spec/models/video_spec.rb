55842040900f2e825850029dc4bb778d36b93594
Tom Lawrence <mrthomasjacoblawrence@hotmail.co.uk>
2026-07-06 01:29:35 +0100

    Test that enum is correctl defined for provider
    1. provider is an enum for the source host of video

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Video, type: :model do
   it "should validate presence of provider" do
     should validate_presence_of(:provider)
   end
+
+  it "should define enum for provider" do
+    should define_enum_for(:provider).with_values(MuxVideo: 0)
+  end
 end
